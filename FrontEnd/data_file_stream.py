ac32b38737295d015a104b9c1d781ea4e37f41d9
Alessandro <ricci.alessandro.alberto@gmail.com>
2021-01-02 12:05:49 +0100

    Add save to file functionality

--- FrontEnd/data_file_stream.py
+++ FrontEnd/data_file_stream.py
@@ -15,19 +15,21 @@ class DataFileStream():
         self.app = _app
         self.directory = None # TODO IN REALTA' QUI CI DEVO METTERE IL PERCORSO BASE DOVE DOVREI TROVARE I DB
         self.save_button = SaveButton(self.main_frame, self)
-        self.load_button = LoadButton(self.main_frame, self)
         self.select_dir = SelectDir(self.main_frame, self)
 
+        if self.app.debug:
+            self.load_button = LoadButton(self.main_frame, self)
         #self.save_button.grid(row=0, sticky=tk.EW, padx=5, pady=1)#rowspan=3
         #self.load_button.grid(row=1, sticky=tk.EW, padx=5, pady=1)
-        self.save_button.pack(fill="x")#, padx=5, pady=1)
-        self.load_button.pack(fill="x")#, padx=5, pady=1)
-        self.select_dir.pack(fill="x")#, padx=5, pady=1)
         
+        self.select_dir.pack(fill="x")#, padx=5, pady=1)
+        self.save_button.pack(fill="x")#, padx=5, pady=1)
+        if self.app.debug:
+            self.load_button.pack(fill="x")#, padx=5, pady=1)
 
 class SaveButton(tk.Button):
     def __init__(self, _master, _data_file_stream):
-        super().__init__(_master, text="SALVA", command=self.on_press)
+        super().__init__(_master, text="SALVA SU FILE", command=self.on_press)
         self.data_file_stream = _data_file_stream 
 
     def on_press(self):
@@ -35,22 +37,72 @@ class SaveButton(tk.Button):
             self.data_file_stream.directory = filedialog.askdirectory(initialdir=".\FrontEnd\DB\Default")
             if self.data_file_stream.directory:
                 print(self.data_file_stream.directory)
-                #LEGGI LE CONFIGURAZIONI --> OUTPUT N_PIN_CONN_DIST (dizonario contenente il nome dei connettori con il numero di pin)
+                #SALVA LA CONFIG
                 try:
-                    pass
+
+                    print()
+                    _path = self.data_file_stream.directory+"\config.xlsx"
+                    writer = pd.ExcelWriter(_path, engine="openpyxl")
+                    _conns = []
+                    _pins = []
+                    for k,v in self.data_file_stream.app.continuity.items():
+                        #trova il numero di pin del connettore
+                        
+                        for _conn in self.data_file_stream.app.connectors:
+                            if _conn.get_name() == k:
+                                _conns.append(_conn.get_name())
+                                _pins.append(_conn.get_n_pin())
+                                #print("OOK", _conn.get_name())
+                                #print("pin", _n_pin)
+                        #_sheet_value = {" ":_col}
+                    _sheet_value = {"conn":_conns, "n_pin":_pins} #prima colonna del excel
+                    df = pd.DataFrame(_sheet_value)
+                    df.to_excel(writer,index=False)
+                    writer.save()
+                except:
+                    print("save config failed")
+                    messagebox.showerror("Error", e)
+                    return
+
+                #SALVA I CONNETTORI NEI RELATIVI FILE
+                try:
+                    for k,v in self.data_file_stream.app.continuity.items():
+                        #trova il numero di pin del connettore
+                        for _conn in self.data_file_stream.app.connectors:
+                            if _conn.get_name() == k:
+                                _n_pin = _conn.get_n_pin()
+                                print("OOK", _conn.get_name())
+                                print("pin", _n_pin)
+                        _col_pin = list(range(1,_n_pin+1,1))
+                        #_sheet_value = {" ":_col}
+                        _sheet_value = {" ":_col_pin} #prima colonna del excel
+                        print("key", k)
+                        _path = self.data_file_stream.directory+"\\"+k+".xlsx"
+                        writer = pd.ExcelWriter(_path, engine="openpyxl")
+                        for kk,vv in v.items(): 
+                            print("key", kk)
+                            for row in vv:
+                                for pin1 in row:
+                                    print(pin1)
+                                print("\n")
+                            print("\n")
+                            vv_transp = list(map(list,zip(*vv)))
+                            j=1
+                            for _col in vv_transp:
+                                _sheet_value[j] = _col #colonne successive dell'excell
+                                j+=1
+                            df = pd.DataFrame(_sheet_value)
+                            df.to_excel(writer,sheet_name=kk,index=False)
+                        writer.save()
                 except Exception as e:
                     messagebox.showerror("Error", e)
                     return
-                #print(type(readed_data["conn"]))
-                #print(readed_data["conn"])
-
-                _keys = []
         else:
             print("nessuna matrice di continuità da salvare")
 
 class SelectDir(tk.Button):
     def __init__(self, _master, _data_file_stream):
-        super().__init__(_master, text="Seleziona Dir.", command=self.on_press)
+        super().__init__(_master, text="APRI FILE", command=self.on_press)
         self.data_file_stream = _data_file_stream 
 
     def on_press(self):
@@ -78,6 +130,7 @@ class SelectDir(tk.Button):
                         #print("nan intercettato")
                         pass
                 else:
+                    print("appeso",c)
                     _keys.append(c)
             _values = []
             #print(type(readed_data["n_pin"]))
@@ -92,8 +145,15 @@ class SelectDir(tk.Button):
                     else:
                         #print("ok")
                         _values.append(int(c))
-            #print(_keys)
-            #print(_values)
+                elif isinstance(c,int):
+                    if math.isnan(c):
+                        #print("nan intercettato")
+                        pass
+                    else:
+                        #print("ok")
+                        _values.append(int(c))
+            print(_keys)
+            print(_values)
             _n_pin_dict = dict(zip(_keys,_values))
             print(_n_pin_dict)
             #n_pin_dict = {"conn1":n_pin_1, "conn2":n_pin_2, "conn3":n_pin_3}
@@ -153,7 +213,7 @@ class SelectDir(tk.Button):
         
 class LoadButton(tk.Button):
     def __init__(self, _master, _data_file_stream):
-        super().__init__(_master,text="APRI ...", command=self.on_press)
+        super().__init__(_master,text="APRI config base", command=self.on_press)
         self.data_file_stream = _data_file_stream
     
     def on_press(self):
